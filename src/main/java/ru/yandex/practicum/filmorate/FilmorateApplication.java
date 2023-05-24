2ebfe5df380c90037e70641ca4143763192854e8
KoltsovaIA <igmtur@yandex.ru>
2023-05-24 08:37:03 +0300

    code style correction

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -2,8 +2,6 @@ package ru.yandex.practicum.filmorate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-
 
 @SpringBootApplication
 public class FilmorateApplication {
